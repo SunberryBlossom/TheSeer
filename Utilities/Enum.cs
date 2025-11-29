a54b5406cf49ae934f55bcf68bbeacdc172e12a5
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-29 16:55:15 +0100

    Create Enum class. Add UserStatus enum

---
+++ Utilities/Enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSeer.Utilities
+{
+    internal class Enum
+    {
+        public enum UserStatus
+        {
+            LoggedIn,
+            LoggedOut,
+        }
+    }
+}
