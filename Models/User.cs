4bf68f91ea024d677b150d066ffbb83b0085cdc6
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-29 16:52:23 +0100

    Add properties, contructor, update methods and getuserinfo method structure to User class

--- Models/User.cs
+++ Models/User.cs
@@ -8,5 +8,49 @@ namespace TheSeer.Models
 {
     internal class User
     {
+        private string Username { get; set; }
+        private string PasswordHash { get; set; }
+        private string Email { get; set; }
+        private string Nickname { get; set; }
+
+        public User(string username, string passwordHash, string email, string nickname)
+        {
+            UpdateUsername(username);
+            UpdatePassword(passwordHash);
+            UpdateEmail(email);
+            UpdateNickname(nickname);
+        }
+
+
+        // ------------- Update methods for user properties -------------- //
+        public void UpdatePassword(string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmail(string newEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateNickname(string newNickname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUsername(string newUsername)
+        {
+            throw new NotImplementedException();
+        }
+
+        // ----------------------------------------------- //
+
+        public void GetUserInfo(string infoWanted)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
     }
 }
