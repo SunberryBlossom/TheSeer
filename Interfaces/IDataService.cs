fc98b30b0421ef7f463b2a8f9b049fc90a234ee0
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-30 16:27:06 +0100

    Add DataService Interface

---
+++ Interfaces/IDataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSeer2.Models;
+using TheSeer2.Models.Enums;
+
+namespace TheSeer2.Interfaces
+{
+    internal interface IDataService
+    {
+        User? GetUser(string username);
+        void SaveUser(User user);
+        List<User> GetAllUsers();
+        List<Reading> GetUserReadings(Guid userId);
+        void SaveReading(ReadingType reading);
+
+    }
+}
