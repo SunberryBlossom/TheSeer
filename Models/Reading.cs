faf09c4911ce5b04a9553075bd3fec3f5b8f386e
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-30 17:22:44 +0100

    Add code to Reading class

--- Models/Reading.cs
+++ Models/Reading.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using TheSeer2.Models.Enums;
 
 namespace TheSeer2.Models
 {
     internal class Reading
     {
+        // ----------------------- PROPERTIES --------------------------//
+        public Guid Id { get; init; }
+        public Guid UserId { get; init; }
+        public ReadingType Type { get; init; }
+        public CardSuit CardSuit { get; init; }
+        public List<Card> Cards { get; init; }
+        public DateTime Timestamp { get; init; }
+
+        // ---------------- CONSTRUCTORS -----------------------------//
+        
+        public Reading(Guid userId, ReadingType type, CardSuit cardSuit, List<Card> cards)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Type = type;
+            CardSuit = cardSuit;
+            
+            if (cards == null || cards.Count == 0)
+                throw new ArgumentException("A reading must have at least one card", nameof(cards));
+            
+            Cards = cards;
+            Timestamp = DateTime.Now;
+        }
+
+        public Reading() 
+        {
+            Cards = new List<Card>();
+        }
     }
 }
