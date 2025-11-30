4807f5d49deb789555e7d4abb335157f51cf9cc9
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-30 16:09:31 +0100

    add interface for encryption

---
+++ Interfaces/ICryptographyService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSeer2.Interfaces
+{
+    internal interface ICryptographyService
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string password, string hashedPassword);
+    }
+}
