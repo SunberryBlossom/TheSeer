8004ac7999e2cbc22f3ad089bbbaaa431ccbab53
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-01 09:22:07 +0100

    refactor to separate between suit and deck

--- Models/Enums/CardSuit.cs
+++ Models/Enums/CardSuit.cs
@@ -4,11 +4,15 @@ using System.Text;
 
 namespace TheSeer2.Models.Enums
 {
-	enum CardSuit
-	{
-		Arcana,
-		Oracle,
-		Angel,
-		Rune
-	}
+    internal enum CardSuit
+    {
+        MajorArcana,
+
+        Wands,
+        Cups,
+        Swords,
+        Pentacles,
+
+        None
+    }
 }
