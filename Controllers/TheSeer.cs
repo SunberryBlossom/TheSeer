56d92caadb3b1ce16e905569b21297606ec75eb6
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-04 16:12:32 +0100

    Create TheSeer controller class

---
+++ Controllers/TheSeer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using TheSeer.Interfaces;
+using TheSeer.Managers;
+using TheSeer.Models;
+using TheSeer.Models.Enums;
+using TheSeer.Services;
+
+namespace TheSeer.Controllers
+{
+    /// <summary>
+    /// Application controller - orchestrates the entire application flow
+    /// </summary>
+    internal class TheSeer
+    {
+        // ----------------------- DEPENDENCIES --------------------------//
+        private readonly UserManager _userManager;
+        private readonly ReadingManager _readingManager;
+        private readonly Narrator _narrator;
+
+        private bool _isRunning;
+
+        // ---------------- CONSTRUCTORS -----------------------------//
+        
+        public TheSeer(
+            UserManager userManager,
+            ReadingManager readingManager,
+            Narrator narrator)
+        {
+            _userManager = userManager;
+            _readingManager = readingManager;
+            _narrator = narrator;
+            _isRunning = false;
+        }
+
+        // --------------- PUBLIC METHODS -----------------------//
+
+        /// <summary>
+        /// Main application entry point - orchestrates the entire flow
+        /// </summary>
+        public void Run()
+        {
+            _isRunning = true;
+            _narrator.Welcome();
+
+            while (_isRunning)
+            {
+                // Authentication loop - allows multiple users to log in
+                if (!AuthenticateUser())
+                {
+                    // User chose to exit from login screen
+                    break;
+                }
+
+                // Main application loop for authenticated user
+                MainLoop();
+
+                // User logged out - loop back to authentication
+                _userManager.Logout();
+            }
+
+            _narrator.Farewell();
+        }
+
+        /// <summary>
+        /// Stops the application entirely
+        /// </summary>
+        public void Stop()
+        {
+            _isRunning = false;
+        }
+
+        // --------------- PRIVATE ORCHESTRATION METHODS -----------------------//
+
+        /// <summary>
+        /// Handles user authentication - returns false if user wants to exit app
+        /// </summary>
+        private bool AuthenticateUser()
+        {
+            // TODO: Create and call AuthenticationMenu
+            // var authMenu = new AuthenticationMenu(_userManager, _narrator);
+            // return authMenu.Show(); // Returns true if logged in, false if user wants to exit
+            
+            return true; // Temporary - remove when AuthenticationMenu is implemented
+        }
+
+        /// <summary>
+        /// Main application loop for authenticated user - delegates to MainMenu
+        /// </summary>
+        private void MainLoop()
+        {
+            bool userWantsToLogout = false;
+
+            while (!userWantsToLogout && _isRunning)
+            {
+                // TODO: Create and call MainMenu
+                // var mainMenu = new MainMenu(this);
+                // userWantsToLogout = mainMenu.Show(); // Returns true if user chooses logout
+                
+                userWantsToLogout = true; // Temporary - remove when MainMenu is implemented
+            }
+        }
+
+        // --------------- SERVICE ACCESS FOR MENUS -----------------------//
+
+        public UserManager UserManager => _userManager;
+        public ReadingManager ReadingManager => _readingManager;
+        public Narrator Narrator => _narrator;
+    }
+}
