c0a4b350c677c90a9c511295eb96b716db65f09f
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-21 15:46:56 +0100

    Removing of data. Experimental branch, code first approach

--- Interfaces/IValidationService.cs
+++
@@ -1,9 +0,0 @@
-namespace TheSeer.Interfaces
-{
-    public interface IValidationService
-    {
-        bool IsValidUsername(string username, out string? errorMessage);
-        bool IsValidPassword(string password, out string? errorMessage);
-        bool IsValidEmail(string email, out string? errorMessage);
-    }
-}
--- Program.cs
+++ Program.cs
@@ -1,33 +1,10 @@
-﻿using System;
-using TheSeer.Controllers;
-using TheSeer.Managers;
-using TheSeer.Services;
-using TheSeer.Utilities.Helpers;
-
-namespace TheSeer
+﻿namespace TheSeer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Initialize all services with dependency injection
-            var dataService = new JsonDataService();
-            var cryptoService = new CryptographyService();
-            var validationService = new ValidationService();
-            var narrator = new Narrator();
-            var tarotService = new TarotService();
-            var spreadService = new SpreadService();
-
-            // Initialize managers
-            var userManager = new UserManager(dataService, cryptoService, validationService);
-            var readingManager = new ReadingManager(tarotService, spreadService, dataService);
-
-            // Create and run the application controller
-            var app = new Controllers.TheSeer(userManager, readingManager, narrator);
-
-
-            app.Run();
-
+            
         }
     }
 }
