29e24ca83fbe3618d1a49abc6938543bd65c953e
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-21 18:36:59 +0100

    Initialize and null-mark all fields correctly

--- Domain/Models/Deck.cs
+++ Domain/Models/Deck.cs
@@ -8,19 +8,19 @@ namespace TheSeer.Domain.Models
     {
         public int Id { get; set; }
         public int SystemTypeId { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         public string? Creator { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } = null!;
         public DateOnly DateOfPublish { get; set; }
-        public string AssetFolder { get; set; }
-        public string CardBackImage { get; set; }
+        public string AssetFolder { get; set; } = null!;
+        public string CardBackImage { get; set; } = null!;
         public bool IsLocked { get; set; }
 
-        public virtual SystemType SystemType { get; set; }
-        public virtual ICollection<Reading> Readings { get; set; }
-        public  virtual ICollection<FavoriteDeck> FavoriteDecks { get; set; }
-        public virtual ICollection<Meaning> Meanings { get; set; }
-        public virtual ICollection<Card> Cards { get; set; }
+        public virtual SystemType SystemType { get; set; } = null!;
+        public virtual ICollection<Reading> Readings { get; set; } = new List<Reading>();
+        public  virtual ICollection<FavoriteDeck> FavoriteDecks { get; set; } = new List<FavoriteDeck>();
+        public virtual ICollection<Meaning> Meanings { get; set; } = new List<Meaning>();
+        public virtual ICollection<Card> Cards { get; set; } = new List<Card>();
 
 
     }
