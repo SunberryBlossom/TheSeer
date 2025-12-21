289bc135aeafaaea17360adb2b9172980a50c4a7
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-21 18:38:24 +0100

    Create DbContext file and add build method for Deck

---
+++ Data/TheSeerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSeer.Domain.Models;
+
+namespace TheSeer.Data
+{
+    internal class TheSeerDbContext : DbContext
+    {
+        public TheSeerDbContext(DbContextOptions<TheSeerDbContext> options) : base(options) { }
+        public DbSet<Deck> Decks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TheSeerDbContext).Assembly);
+        }
+    }
+}
