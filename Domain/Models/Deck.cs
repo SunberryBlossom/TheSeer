1e711194ab05c0badad240b9ee72661176496f78
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-21 16:10:32 +0100

    First draft of fields and relation entities

--- Domain/Models/Deck.cs
+++ Domain/Models/Deck.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace TheSeer.Domain.Models
 {
-    internal class Deck
+    public class Deck
     {
+        public int Id { get; set; }
+        public int SystemTypeId { get; set; }
+        public string Name { get; set; }
+        public string? Creator { get; set; }
+        public string Description { get; set; }
+        public DateOnly DateOfPublish { get; set; }
+        public string AssetFolder { get; set; }
+        public string CardBackImage { get; set; }
+        public bool IsLocked { get; set; }
+
+        public virtual ICollection<Reading> Readings { get; set; }
+        public  virtual ICollection<FavoriteDeck> FavoriteDecks { get; set; }
+        public virtual SystemType SystemType { get; set; }
+        public virtual ICollection<Meaning> Meanings { get; set; }
+        public virtual ICollection<Card> Cards { get; set; }
+
+
     }
 }
