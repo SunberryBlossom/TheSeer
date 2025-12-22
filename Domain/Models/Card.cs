3b8e8d8dad8dc9d7a73c53b60421332b7c032425
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-22 18:16:29 +0100

    Init version of model file

---
+++ Domain/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSeer.Domain.Models
+{
+    internal class Card
+    {
+        public int Id { get; set; }
+        public int DeckId { get; set; }
+        public string Name { get; set; } = null!;
+        public int Value { get; set; }
+        public string SuitOrGroup { get; set; } = null!;
+        public string ImageKey { get; set; } = null!;
+
+        public Deck Deck { get; set; } = null!;
+        public ICollection<DrawnCard> DrawnCards { get; set; } = new List<DrawnCard>();
+        public ICollection<Meaning> Meanings { get; set; } = new List<Meaning>();
+
+    }
+}
---
+++ Domain/Models/SystemType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSeer.Domain.Models
+{
+    internal class SystemType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public bool CanBeReversed { get; set; }
+        public int DefaultCardCount { get; set; }
+
+        public ICollection<Deck> Decks { get; set; } = new List<Deck>();
+        public ICollection<Spread> Spreads { get; set; } = new List<Spread>();
+    }
+}
