3cef092c15a9a81b0421b31f55bf487931bf6a97
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-22 12:21:47 +0100

    Add config file for Drawn cards

---
+++ Data/Configurations/DrawnCardConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSeer.Domain.Models;
+
+namespace TheSeer.Data.Configurations
+{
+    internal class DrawnCardConfiguration : IEntityTypeConfiguration<DrawnCard>
+    {
+        public void Configure(EntityTypeBuilder<DrawnCard> builder)
+        {
+            // Composite key
+            builder.HasKey(dc => new { dc.ReadingId, dc.SpreadPositionId });
+
+            builder.HasIndex(dc => new { dc.ReadingId, dc.CardId }).IsUnique();
+
+            builder.Property(dc => dc.IsReversed).IsRequired();
+            builder.Property(dc => dc.DrawOrder).IsRequired();
+
+            builder.HasOne(dc => dc.Reading).WithMany(r => r.DrawnCards).HasForeignKey(dc => dc.ReadingId).IsRequired();
+            builder.HasOne(dc => dc.SpreadPosition).WithMany(sp => sp.DrawnCards).HasForeignKey(dc => dc.SpreadPositionId).IsRequired();
+            builder.HasOne(dc => dc.Card).WithMany(c => c.DrawnCards).HasForeignKey(dc => dc.CardId).IsRequired();
+        }
+    }
+}
