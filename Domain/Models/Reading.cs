4878a3288f0198183fe96358a0e0d98258547f1a
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-22 10:49:39 +0100

    Add Reading class

---
+++ Domain/Models/Reading.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSeer.Domain.Models
+{
+    internal class Reading
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int DeckId { get; set; }
+        public int SpreadId { get; set; }
+        public DateTime PerformedAt { get; set; }
+        public string? UserMood { get; set; }
+        public string Summary { get; set; } = null!;
+
+        public User User { get; set; } = null!;
+        public Deck Deck { get; set; } = null!;
+        public Spread Spread { get; set; } = null!;
+
+        public ICollection<JournalEntry> JournalEntries { get; set; } = new List<JournalEntry>();
+        public ICollection<DrawnCard> DrawnCards { get; set; } = new List<DrawnCard>();
+    }
+}
