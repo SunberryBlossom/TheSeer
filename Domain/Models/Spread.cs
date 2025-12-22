a48980a541eaaf66f8b3e2dc4fa23f0314f908ea
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-22 11:19:55 +0100

    Add Spread model

---
+++ Domain/Models/Spread.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSeer.Domain.Models
+{
+    internal class Spread
+    {
+        public int Id { get; set; }
+        public int SystemTypeId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string? ImageOverlay  { get; set; }
+
+        public SystemType SystemType { get; set; }
+        public ICollection<Reading> Readings { get; set; } = new List<Reading>();
+        public ICollection<SpreadPosition> SpreadPositions { get; set; } = new List<SpreadPosition>();
+
+    }
+}
