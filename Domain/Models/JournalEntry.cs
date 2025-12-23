775e43d9bdf4344514f74f85a49a56239474f351
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-23 17:34:13 +0100

    init file

---
+++ Domain/Models/JournalEntry.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSeer.Domain.Models
+{
+    internal class JournalEntry
+    {
+        public Guid Id { get; set; }
+        public Guid ReadingId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime DateWritten { get; set; }
+        public string Header { get; set; } = null!;
+        public string Content { get; set; } = null!;
+
+        public Reading Reading { get; set; } = null!;
+        public User User { get; set; } = null!;
+    }
+}
---
+++ Domain/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSeer.Domain.Models
+{
+    internal class User
+    {
+        public Guid Id { get; set; }
+        public string HashedPassword { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string? DisplayName { get; set; }
+        public string Email { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastLoginAt { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string Role { get; set; } = null!;
+    }
+}
