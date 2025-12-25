dc34b1d95b10b9917157a8e91abfd16d575d662e
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-25 16:56:38 +0100

    Add connection string and host build

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace TheSeer
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using TheSeer.Data;
+
+namespace TheSeer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<TheSeerDbContext>(options => options.UseSqlServer(connectionString));
+
+            using IHost host = builder.Build();
         }
     }
 }
