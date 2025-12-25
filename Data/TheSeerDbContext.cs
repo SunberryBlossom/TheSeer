230156525ee972ff7fd4522895cefe42751e101d
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-25 17:28:48 +0100

    Add model create for two tarot decks

--- Data/TheSeerDbContext.cs
+++ Data/TheSeerDbContext.cs
@@ -32,14 +32,58 @@ namespace TheSeer.Data
 
             modelBuilder.Entity<SystemType>().HasData
                 (
-                    new SystemType { Id = 1, Name = "Tarot", CanBeReversed = true, DefaultCardCount = 78, Description = "The classical system for introspection, guidance and spirital development. Usually has 78 cards, always divided into the major and minor arcana." },
-                    new SystemType { Id = 2, Name = "Runes", CanBeReversed = true, DefaultCardCount = 24, Description = "An old norse system built upon the old futhark of 24 runes. Each rune symbolize natural powers and gods or godesses. Used to interpret hidden energies and bigger happenings in life." },
-                    new SystemType { Id = 3, Name = "Oracle", CanBeReversed = false, DefaultCardCount = 44, Description = "A free and intuitive system for guidance. Not as strict as the Tarot system. Usually have unique themes, and can be flexible regarding card count." }
+                    new SystemType
+                    {
+                        Id = 1,
+                        Name = "Tarot",
+                        CanBeReversed = true,
+                        DefaultCardCount = 78,
+                        Description = "The classical system for introspection, guidance and spirital development. Usually has 78 cards, always divided into the major and minor arcana."
+                    },
+                    new SystemType
+                    {
+                        Id = 2,
+                        Name = "Runes",
+                        CanBeReversed = true,
+                        DefaultCardCount = 24,
+                        Description = "An old norse system built upon the old futhark of 24 runes. Each rune symbolize natural powers and gods or godesses. Used to interpret hidden energies and bigger happenings in life."
+                    },
+                    new SystemType
+                    {
+                        Id = 3,
+                        Name = "Oracle",
+                        CanBeReversed = false,
+                        DefaultCardCount = 44,
+                        Description = "A free and intuitive system for guidance. Not as strict as the Tarot system. Usually have unique themes, and can be flexible regarding card count."
+                    }
                 );
 
             modelBuilder.Entity<SystemType>().HasData
                 (
-                    new Deck {Id = 1, Name = "The Seer's own deck", SystemTypeId = 1, Creator = "Elvira Mariesdotter", IsLocked = false, Description = "The deck the seer herself carved out of the sandstone in her lands. Carries specific suits and meanings. Can create somewhat harsh readings, but always true and with good intent." }
+                    new Deck
+                    {
+                        Id = 1,
+                        Name = "The Seer's deck",
+                        SystemTypeId = 1,
+                        Creator = "Elvira Mariesdotter",
+                        IsLocked = false,
+                        Description = "The deck the seer herself carved out of the sandstone in her lands. Carries specific suits and meanings. Can create somewhat harsh readings, but always true and with good intent.",
+                        AssetFolder = " ",
+                        CardBackImage = " ",
+                        DateOfPublish = new DateOnly(2025, 12, 25)
+                    },
+                    new Deck
+                    {
+                        Id = 2,
+                        Name = "Lord of the Rings Tarot",
+                        SystemTypeId = 1,
+                        Creator = "Casey Gilly",
+                        IsLocked = false,
+                        Description = "A Lord of the Rings inspired tarot deck, replacing the pentacle suit with rings instead. All Major arcana and clothed cards are named after characters from the Realm of J.R.R. Tolkien. Illustrations by Tomas Hijo.",
+                        AssetFolder = " ", 
+                        CardBackImage = " ", 
+                        DateOfPublish = new DateOnly(2022, 08, 28)
+                    }
                 );
         }
     }
