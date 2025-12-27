6c3151cca8940f5a5dd08786881ccab1a29196cb
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-27 13:39:49 +0100

    Create ICard repo and IGeneric repo

---
+++ Data/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace TheSeer.Data.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
