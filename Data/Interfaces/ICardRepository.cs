d28dc0cded906372809a25a1db703370a9beff3d
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-27 15:27:31 +0100

    Add card interface for repo

--- Data/Interfaces/ICardRepository.cs
+++ Data/Interfaces/ICardRepository.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TheSeer.Domain.Models;
 
 namespace TheSeer.Data.Interfaces
 {
-    internal interface ICardRepository
+    public interface ICardRepository : IGenericRepository<Card>
     {
+        Card GetById(int id);
+
+        IQueryable<Card> GetAllWithMeanings();
     }
 }
