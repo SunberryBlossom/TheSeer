f0fe507f77714dd4f0e7bc5f191fce4284dde780
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-27 15:27:05 +0100

    Add Card and generic repository

---
+++ Data/Repositories/CardRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSeer.Data.Interfaces;
+using TheSeer.Domain.Models;
+
+namespace TheSeer.Data.Repositories
+{
+    public class CardRepository : GenericRepository<Card>, ICardRepository
+    {
+
+        public CardRepository(TheSeerDbContext context) : base(context)
+        {
+
+        }
+
+        public Card GetById(int id) => _dbSet.Find(id);
+        public IQueryable<Card> GetAllWithMeanings() => _dbSet.Include(c => c.Meanings);
+    }
+}
---
+++ Data/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using TheSeer.Data.Interfaces;
+
+namespace TheSeer.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public void Add(T entity) => _dbSet.Add(entity);
+        public void AddRange(IEnumerable<T> entities) => _dbSet.AddRange(entities);
+        public void Update(T entity) => _dbSet.Update(entity);
+        public void Remove(T entity) => _dbSet.Remove(entity);
+        public void RemoveRange(IEnumerable<T> entities) => _dbSet.RemoveRange(entities);
+    }
+}
