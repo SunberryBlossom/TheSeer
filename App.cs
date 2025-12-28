bbb29826102b645f638299b1a8bf05545c11a4ce
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-28 19:38:33 +0100

    Add App class for later implementation

--- App.cs
+++ App.cs
@@ -1,10 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using TheSeer.Business.DTOs;
+using TheSeer.Business.Interfaces;
 
-namespace TheSeer
+namespace TheSeer.ConsoleApp
 {
-    internal class App
+    public class App
     {
+        private readonly IUserService _userService;
+        private readonly ICatalogService _catalogService;
+        private readonly IReadingService _readingService;
+        private UserSessionDto? _currentUser;
+
+        public App(IUserService userService, ICatalogService catalogService, IReadingService readingService)
+        {
+            _userService = userService;
+            _catalogService = catalogService;
+            _readingService = readingService;
+        }
+
+        public void Run()
+        {
+
+            bool running = true;
+            while (running)
+            {
+                // NOT YET IMPLEMENTED! This will be the final step I think, after views and menus >:3
+                running = false;
+            }
+        }
     }
 }
