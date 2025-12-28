bb03d040bc673441ebaa9346edd119461887baa9
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-28 14:06:54 +0100

    Add Spread Position repo

---
+++ Data/Repositories/SpreadPositionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSeer.Data.Interfaces;
+using TheSeer.Domain.Models;
+
+namespace TheSeer.Data.Repositories
+{
+    internal class SpreadPositionRepository : GenericRepository<SpreadPosition>, ISpreadPositionRepository
+    {
+        public SpreadPositionRepository(TheSeerDbContext context) : base(context)
+        {
+        }
+
+        public SpreadPosition GetById(int id) => _dbSet.Find(id);
+        public IQueryable<SpreadPosition> GetPositionsBySpread(int spreadId) => _dbSet.Where(sp => sp.SpreadId == spreadId).OrderBy(sp => sp.SequenceNumber);
+    }
+}
