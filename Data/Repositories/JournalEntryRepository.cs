d74332df037f0bce42715c8f91352b2db3a2254e
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-28 14:38:28 +0100

    Add Journal entry repo

---
+++ Data/Repositories/JournalEntryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSeer.Data.Interfaces;
+using TheSeer.Domain.Models;
+
+namespace TheSeer.Data.Repositories
+{
+    internal class JournalEntryRepository : GenericRepository<JournalEntry>, IJournalEntryRepository
+    {
+        public JournalEntryRepository(TheSeerDbContext context) : base(context)
+        {
+        }
+
+        public JournalEntry GetById(Guid id) => _dbSet.Find(id);
+        public IQueryable<JournalEntry> GetUserJournal(Guid userId) => _dbSet.Where(j => j.UserId == userId).OrderByDescending(j => j.DateWritten);
+        public JournalEntry GetEntryByReading(Guid readingId) =>_dbSet.FirstOrDefault(j => j.ReadingId == readingId);
+    }
+}
