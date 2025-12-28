be45432572c37644e0572b5252255336de5b14d3
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-28 13:47:11 +0100

    Add Spread repository

---
+++ Data/Repositories/SpreadRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSeer.Data.Interfaces;
+using TheSeer.Domain.Models;
+
+namespace TheSeer.Data.Repositories
+{
+    internal class SpreadRepository : GenericRepository<Spread>, ISpreadRepository
+    {
+        public SpreadRepository(TheSeerDbContext context) : base(context)
+        {
+        }
+
+        public Spread GetById(int id) => _dbSet.Find(id);
+        public IQueryable<Spread> GetSpreadsBySystem(int systemTypeId) => _dbSet.Where(s => s.SystemTypeId == systemTypeId);
+        public Spread GetSpreadWithPositions(int id) => _dbSet.Include(s => s.SpreadPositions).FirstOrDefault(s => s.Id == id);
+
+    }
+}
