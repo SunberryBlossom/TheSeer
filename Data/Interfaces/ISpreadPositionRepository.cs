9d1b9717f46acaacd9a99fa6c8b0326a2223f954
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-28 15:24:24 +0100

    Fixed typo in interface class type

--- Data/Interfaces/ISpreadPositionRepository.cs
+++ Data/Interfaces/ISpreadPositionRepository.cs
@@ -5,7 +5,7 @@ using TheSeer.Domain.Models;
 
 namespace TheSeer.Data.Interfaces
 {
-    internal interface ISpreadPositionRepository : IGenericRepository<Spread>
+    internal interface ISpreadPositionRepository : IGenericRepository<SpreadPosition>
     {
         SpreadPosition GetById(int id);
         IQueryable<SpreadPosition> GetPositionsBySpread(int spreadId);
