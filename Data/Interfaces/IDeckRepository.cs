31f0a011a2e7ed5cdda7d9acd3222d3181ea91da
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-28 12:36:20 +0100

    Add Deck repository interface

---
+++ Data/Interfaces/IDeckRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSeer.Domain.Models;
+
+namespace TheSeer.Data.Interfaces
+{
+    internal interface IDeckRepository : IGenericRepository<Deck>
+    {
+        Deck GetById(int id);
+        Deck GetDeckWithCards(int id);
+        IQueryable<Deck> GetDecksByType(int systemTypeId);
+    }
+}
