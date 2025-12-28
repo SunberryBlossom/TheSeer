7c3362deb9445e201965b6656470d8600b501563
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-28 15:16:57 +0100

    Add interface for UOW

---
+++ Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSeer.Data.Interfaces
+{
+    internal interface IUnitOfWork : IDisposable
+    {
+        ICardRepository Cards { get; }
+        IDeckRepository Decks { get; }
+        ISystemTypeRepository SystemTypes { get; }
+        ISpreadRepository Spreads { get; }
+        ISpreadPositionRepository SpreadPositions { get; }
+        IMeaningRepository Meanings { get; }
+        IDrawnCardRepository DrawnCards { get; }
+        IReadingRepository Readings { get; }
+        IUserRepository Users { get; }
+        IFavoriteDeckRepository FavoriteDecks { get; }
+        IJournalEntryRepository JournalEntries { get; }
+
+        int Save();
+        void BeginTransaction();
+        void Commit();
+        void Rollback();
+    }
+}
