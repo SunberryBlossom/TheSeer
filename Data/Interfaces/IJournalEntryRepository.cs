b5968a43dfffc4bbe52ff9ef69f2acdc8d2c93f6
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-28 14:37:00 +0100

    Add interface for JournalEntry repo

---
+++ Data/Interfaces/IJournalEntryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSeer.Domain.Models;
+
+namespace TheSeer.Data.Interfaces
+{
+    internal interface IJournalEntryRepository : IGenericRepository<JournalEntry>
+    {
+        JournalEntry GetById(Guid id);
+        IQueryable<JournalEntry> GetUserJournal(Guid userId);
+        JournalEntry GetEntryByReading(Guid readingId);
+    }
+}
