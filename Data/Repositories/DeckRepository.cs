eb45823abbf42df643d97c1f97dc4f8b680a9633
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-28 12:37:24 +0100

    Add Deck repository

---
+++ Data/Repositories/DeckRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSeer.Data.Interfaces;
+using TheSeer.Domain.Models;
+
+namespace TheSeer.Data.Repositories
+{
+    internal class DeckRepository : GenericRepository<Deck>, IDeckRepository 
+    {
+        public DeckRepository(TheSeerDbContext context) : base(context)
+        {
+        }
+
+        public Deck GetById(int id) => _dbSet.Find(id);
+        public Deck GetDeckWithCards(int id) =>  _dbSet.Include(d => d.Cards).FirstOrDefault(d => d.Id == id);  
+        public IQueryable<Deck> GetDecksByType(int systemTypeId) =>  _dbSet.Where(d => d.SystemTypeId == systemTypeId);
+        
+
+    }
+}
