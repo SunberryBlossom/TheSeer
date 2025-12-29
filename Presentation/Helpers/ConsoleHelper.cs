e94bc1d04ba38a851719f1bca40e9c3275e824a8
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-29 21:15:32 +0100

    Update formatting of header

--- Presentation/Helpers/ConsoleHelper.cs
+++ Presentation/Helpers/ConsoleHelper.cs
@@ -35,10 +35,36 @@ namespace TheSeer.Presentation.Helpers
 
         public static void Header(string title)
         {
-            Console.Clear();
-            WriteLine(new string('=', 55), Primary);
-            Type($"        ::: {title.ToUpper()} :::", Secondary, 40);
-            WriteLine(new string('=', 55), Primary);
+            int width = 55;
+            string eye = "|";
+            string spice = "~";
+            string top = $"{spice}{new string('═', width - 2)}{spice}";
+            string bottom = $"{spice}{new string('═', width - 2)}{spice}";
+            string centeredTitle = $" {eye}  {title.ToUpper()}  {eye} ";
+            int pad = (width - centeredTitle.Length) / 2;
+            string titleLine = new string(' ', pad) + centeredTitle + new string(' ', width - pad - centeredTitle.Length);
+
+            WriteLine(top, Accent);
+
+            for (int i = 0; i < titleLine.Length; i++)
+            {
+                if (titleLine.Substring(i).StartsWith(eye))
+                {
+                    Console.ForegroundColor = Accent;
+                    Console.Write(eye);
+                    Console.ResetColor();
+                    i += eye.Length - 1;
+                }
+                else
+                {
+                    Console.ForegroundColor = Accent;
+                    Console.Write(titleLine[i]);
+                }
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+
+            WriteLine(bottom, Accent);
             Console.WriteLine();
         }
 
